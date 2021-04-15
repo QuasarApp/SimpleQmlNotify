e62ea7035a2cbfbdcdc9b3144b4a66d395dd9ea1
EndrII <EndrIIMail@gmail.com>
2021-04-15 15:56:38 +0300

    fix documentation

--- notificationdata.h
+++ notificationdata.h
@@ -23,9 +23,15 @@ class NOTIFYSERVICESHARED_EXPORT NotificationData
 
 public:
 
+    /**
+     * @brief The Type enum This enum contatins all available types of the notification messages.
+     */
     enum Type {
+        /// This is message for general notification.
         Normal,
+        /// This is warning notification.
         Warning = 1,
+        /// This is critical error notifications.
         Error = 2,
     };
 
@@ -34,10 +40,34 @@ public:
                               const QString& img = "",
                               int type = Type::Normal);
 
+    /**
+     * @brief text This method return main text message of a question/notification object.
+     * @return plain text of message.
+     */
     Q_INVOKABLE QString text() const;
+
+    /**
+     * @brief img This method return url to image of a question/notification object.
+     * @return url of image.
+     */
     Q_INVOKABLE QString img() const;
+
+    /**
+     * @brief title This method return title of a question/notification object.
+     * @return title of a question/notification object.
+     */
     Q_INVOKABLE QString title() const;
+
+    /**
+     * @brief type This method return type of the notification message or code of the question.
+     * @return type of the notification message or code of the question.
+     */
     Q_INVOKABLE int type() const;
+
+    /**
+     * @brief isValid This method return true if this object is valid.
+     * @return return true if this object is valid.
+     */
     Q_INVOKABLE bool isValid() const;
 
     bool operator ==(const NotificationData &righ);
--- notificationservice.h
+++ notificationservice.h
@@ -21,26 +21,55 @@ class NOTIFYSERVICESHARED_EXPORT NotificationService: public QObject
 
 public:
     /**
-     * @brief Notify.
-     * @return notyfyData for qml.
+     * @brief Notify This method return data of the last notify message.
+     * @return return data of the last notify message.
      */
     NotificationData notify() const;
+
+    /**
+     * @brief question This method return data of the last question.
+     * @return question data.
+     */
     NotificationData question() const;
 
     /**
-     * @brief setNotify - Add new message for application.
-     * @param notify - Message data.
+     * @brief setNotify This method add new notification message of application.
+     * @param notify are message data.
      */
     void setNotify(const NotificationData &notify);
 
+    /**
+     * @brief setQuestion This method sets information of the users question.
+     * @param question are data of the question.
+     */
     void setQuestion(const NotificationData& question);
+
+    /**
+     * @brief questionComplete This method is main responce method of the users questions.
+     * @param accepted are boolean option. Set this option to true if user click on accept button.
+     * @param code are automaticly generated code fo the question.
+     */
     Q_INVOKABLE void questionComplete(bool accepted, int code = 0);
 
+    /**
+     * @brief setNotify This method set new notify message for user. When notify changed on main application windows shows notify message.
+     * @param title are title of a notify window.
+     * @param text are text value of a notify window.
+     * @param img are url to image of a notify window.
+     * @param type are code of a notification type. For more information about notification type see the NotificationData enum.
+     */
     Q_INVOKABLE void setNotify(const QString& title = "",
                        const QString& text = "",
                        const QString& img = "",
                        int type = NotificationData::Normal);
 
+    /**
+     * @brief setQuestion This method set new question for user. When question changed on main application windows shows question.
+     * @param title are title of a question window.
+     * @param text are text value of a question window.
+     * @param img are url to image of a queston window.
+     * @param code are code of question. This code must be sendet to the questionComplete method after buttons clik.
+     */
     Q_INVOKABLE void setQuestion(const QString& title = "",
                        const QString& text = "",
                        const QString& img = "",
@@ -48,8 +77,8 @@ public:
 
 
     /**
-     * @brief getService - This method return instance to notify service. 
-     * @return pointer to service.
+     * @brief getService This method return instance to notify service.
+     * @return pointer to the notification service.
      */
     static NotificationService* getService();
 
@@ -60,8 +89,22 @@ public:
     const QList<NotificationData> & history() const;
 
 signals:
+    /**
+     * @brief notifyChanged This signal emited whet the notificator (Ths object) received a new notification message.
+     */
     void notifyChanged();
+
+    /**
+     * @brief questionChanged This signal emited whet the notificator (Ths object) received a new question message.
+     */
     void questionChanged();
+
+    /**
+     * @brief questionCompleted This signal emited when youser clcik any buttons on question popup.
+     * @param accepted are bool option with "true" value if the yesr click to accept button.
+     * @param questionCode are automaticly generated code of the question.
+     *  For more information about code option see the setQuestion method.
+     */
     void questionCompleted(bool accepted, int questionCode);
 
 private:
