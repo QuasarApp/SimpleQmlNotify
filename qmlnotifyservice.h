425e3f84e35d3014b4d6253b0c05d3490090c6cf
Oleg-designer <sweetletal0@gmail.com>
2021-04-16 09:49:55 +0300

    ref #5 qmlnotifyservice.h fix

--- qmlnotifyservice.h
+++ qmlnotifyservice.h
@@ -9,18 +9,19 @@ class QQmlApplicationEngine;
 /**
  * Simple notify service for qml.
  * Use :
+ * @code cpp 
  * #include <qmlnotifyservice.h>
- *
  * QmlNotificationService::init();
  * auto service = QmlNotificationService::NotificationService::getService()
  * service->setNotify("title", "text", "UrlOfImage", NotificationData::Normal)
- *
+ * @endcode 
  *
  * in qml :
- *
+ * @code cpp 
  *  NotificationServiceView {
         anchors.fill: parent;
     }
+ * @endcode   
  */
 namespace QmlNotificationService {
     bool NOTIFYSERVICESHARED_EXPORT init(QQmlApplicationEngine *engine);
