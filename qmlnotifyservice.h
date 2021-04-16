79478790edf3086bea2acfb3d9d78842c7c95734
Oleg-designer <sweetletal0@gmail.com>
2021-04-16 10:14:13 +0300

    ref #5 add examples in qmlnotifyservice.h

--- qmlnotifyservice.h
+++ qmlnotifyservice.h
@@ -21,6 +21,46 @@ class QQmlApplicationEngine;
  *  NotificationServiceView {
         anchors.fill: parent;
     }
+ * @endcode
+ * Questions
+ * CPP
+ * @code cpp
+ #include <qmlnotifyservice.h>
+
+ int main() {
+     QmlNotificationService::init();
+     auto service = QmlNotificationService::NotificationService::getService();
+     int questionCode = service->setQuestion("title", "some text");
+
+     QObject::connect(service, QmlNotificationService::NotificationService::questionCompleted,
+                     [questionCode](bool accepted, int questionCode) {
+                         if (accepted && code === questionCode) {
+                            // your action here. 
+                         }
+                     })
+
+     
+ }
+ * @endcode
+ * QML
+ * @code qml
+  NotificationServiceView {
+     anchors.fill: parent;
+ }
+ 
+readonly property int questionCode: 0;
+
+questionCode = notificationService.setQuestion(qsTr("Remove %0 user").arg(userModel.userId),
+                                qsTr("All saved data and records will be delete, Do you want continuee?"))
+Connections {
+    target: notificationService
+    function onQuestionCompleted(accepted, code) {
+        if (accepted && code === privateRoot.questionCode) {
+            if (userModel)
+                backEnd.removeUser(userModel.userId)
+        }
+    }
+}
  * @endcode   
  */
 namespace QmlNotificationService {
