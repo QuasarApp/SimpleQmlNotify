48a1b6fd222fb545c53c3ec37afc89c8c6ab3f3a
EndrII <EndrIIMail@gmail.com>
2021-05-05 16:12:50 +0300

    added suport languages

---
+++ qmlNotify_languages/ru.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>NotificationForm</name>
+    <message>
+        <location filename="../NotifyModule/NotificationForm.qml" line="16"/>
+        <location filename="../NotifyModule/NotificationForm.qml" line="18"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
